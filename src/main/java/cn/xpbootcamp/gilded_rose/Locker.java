034c74678bb4eb83617c8145ff5a396f47e3988e
hongyu.li <hongyu.li@thoughtworks.com>
2020-03-25 14:44:29 +0800

    [feat]: given a locker with one empty room when save a bag then user would get a ticket

--- src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Locker.java
@@ -8,6 +8,15 @@ import java.util.Map;
 public class Locker {
 
     private Map<Ticket, Bag> storage = new HashMap<>();
+    private int size;
+
+    public Locker(int size) {
+        this.size = size;
+    }
+
+    public Locker() {
+        this(19);
+    }
 
     public Ticket saveBag(Bag bag) {
         Ticket ticket = new Ticket();
--- src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerTest.java
@@ -13,4 +13,12 @@ class LockerTest {
         Ticket ticket = locker.saveBag(bag);
         assertNotNull(ticket);
     }
+
+    @Test
+    void should_get_a_ticket_given_a_locker_with_one_empty_room_when_save_bag() {
+        Locker locker = new Locker(1);
+        Bag bag = new Bag();
+        Ticket ticket = locker.saveBag(bag);
+        assertNotNull(ticket);
+    }
 }
