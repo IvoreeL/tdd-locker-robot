1c817f5ec7ab3a14f13da3349c312afb92b97317
hongyu.li <hongyu.li@thoughtworks.com>
2020-04-02 14:27:32 +0800

    [feat]: given a robot who manages two full lockers when save a bag the user would get an error message.

--- src/test/java/cn/xpbootcamp/gilded_rose/LockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/LockerRobotTest.java
@@ -6,14 +6,8 @@ import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-// given a robot who manages two empty lockers(capacity: 1) when save a bag then user would get a ticket and the bag would be saved in locker 1
-// given a robot who manages one full locker(capacity: 1, locker 1) and a empty locker(capacity: 1, locker 2) when save a bag then user would get a ticket and the bag would be saved in locker 2
-// given a robot who manages two empty lockers(capacity: 1) when save two bags then user would get two different tickets and the bags would be saved in order.
-// given a robot who manages two full lockers(capacity: 1) when save a bag the user would get an error message.
-// given a valid ticket when retrieves bag then user would get the bag.
-// given an invalid ticket when retrieves bag then user would get error message('票据无效').
-// given an used ticket when retrieves bag then user would get error message('票据无效').
-// given a robot who manages one full locker(capacity: 1, locker 1) and a empty locker(capacity: 1, locker 2) when retrieve the bag in locker 1 and save a bag then user would get a ticket and the bag would be saved in locker 1
+
+
 
 class LockerRobotTest {
 
@@ -33,13 +27,12 @@ class LockerRobotTest {
     @Test
     void should_get_a_ticket_and_the_bag_in_locker_2_when_save_a_bag_given_a_robot_with_full_locker_and_empty_locker() throws LockerFullException {
         LockerRobot lockerRobot = LockerRobotHelper.createDefaultLockerRobot();
+        lockerRobot.save(new Bag());
         Bag bag = new Bag();
-        Bag anotherBag = new Bag();
-        lockerRobot.save(bag);
-        Ticket ticket = lockerRobot.save(anotherBag);
+        Ticket ticket = lockerRobot.save(bag);
         Locker locker2 = LockerRobotHelper.getLocker(NUMBER_TWO);
         assertNotNull(ticket);
-        assertTrue(locker2.contains(anotherBag));
+        assertTrue(locker2.contains(bag));
     }
 
     @Test
@@ -54,5 +47,19 @@ class LockerRobotTest {
         assertNotEquals(ticket, anotherTicket);
         assertTrue(locker1.contains(bag));
         assertTrue(locker2.contains(anotherBag));
+    }
+
+// given a valid ticket when retrieves bag then user would get the bag.
+// given an invalid ticket when retrieves bag then user would get error message('票据无效').
+// given an used ticket when retrieves bag then user would get error message('票据无效').
+// given a robot who manages one full locker(capacity: 1, locker 1) and a empty locker(capacity: 1, locker 2) when retrieve the bag in locker 1 and save a bag then user would get a ticket and the bag would be saved in locker 1
+
+
+    @Test
+    void should_get_error_message_when_save_a_bag_given_a_robot_who_manages_2_full_lockers() throws LockerFullException {
+        LockerRobot lockerRobot = LockerRobotHelper.createDefaultLockerRobot();
+        lockerRobot.save(new Bag());
+        lockerRobot.save(new Bag());
+        assertThrows(LockerFullException.class, () -> lockerRobot.save(new Bag()), "柜子已满");
     }
 }
