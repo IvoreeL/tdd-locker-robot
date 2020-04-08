45d32721bf6ef7c2fcd72ecf5ba4277ded5207bb
hongyu.li <hongyu.li@thoughtworks.com>
2020-04-08 17:06:06 +0800

    [feat]: given a robot which manages two full lockers when user saves a bag then the user would get an error message

--- src/test/java/cn/xpbootcamp/gilded_rose/SmartLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SmartLockerRobotTest.java
@@ -44,7 +44,6 @@ class SmartLockerRobotTest {
         assertTrue(locker2.hasBag(bag));
     }
 
-    //    given a robot which manages one empty locker and another full locker(locker 1 capacity: 1, locker 2 capacity: 1) when user saves a bag then the user would get a ticket and then the bag would be saved in locker 1
 //    given a robot which manages two full lockers(capacity: 1) when user saves a bag then the user would get an error message('柜子已满')
 //    given a valid ticket when retrieves bag then user would get the bag.
 //    given an invalid ticket when retrieves bag then user would get error message('票据无效').
@@ -65,5 +64,16 @@ class SmartLockerRobotTest {
         smartLockerRobot.save(bag);
         Locker locker1 = LockerRobotHelper.getLocker(NUMBER_ONE);
         assertTrue(locker1.hasBag(bag));
+    }
+
+    @Test
+    void should_get_error_message_when_save_bag_given_a_robot_with_two_full_lockers() {
+        SmartLockerRobot smartLockerRobot = (SmartLockerRobot) LockerRobotHelper.createDefaultSmartLockerRobot()
+                .withLocker(1)
+                .withLocker(1)
+                .build();
+        smartLockerRobot.save(new Bag());
+        smartLockerRobot.save(new Bag());
+        assertThrows(LockerFullException.class, ()->smartLockerRobot.save(new Bag()), "柜子已满");
     }
 }
