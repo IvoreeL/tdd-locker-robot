0def46b9123e78ee2608186d03d297a7ef2e1a26
hongyu.li <hongyu.li@thoughtworks.com>
2020-04-08 17:03:25 +0800

    [feat]: given a robot which manages one empty locker and another full locker when user saves a bag then the user would get a ticket and then the bag would be saved in locker 1

--- src/main/java/cn/xpbootcamp/gilded_rose/SmartLockerRobot.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/SmartLockerRobot.java
@@ -18,8 +18,10 @@ public class SmartLockerRobot implements LockerRobot{
         return availableLocker.save(bag);
     }
 
-    @Override
     public Bag retrieve(Ticket ticket) {
-        return null;
+        Locker storedLocker = inChargedLockers.stream()
+                .filter(locker -> locker.hasTicket(ticket))
+                .findFirst().orElseThrow(InvalidTicketException::new);
+        return storedLocker.retrieve(ticket);
     }
 }
--- src/test/java/cn/xpbootcamp/gilded_rose/SmartLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SmartLockerRobotTest.java
@@ -21,12 +21,6 @@ class SmartLockerRobotTest {
         assertTrue(locker1.hasBag(bag));
     }
 
-//    given a robot which manages two empty lockers(locker 1 capacity: 1, locker 2 capacity: 2) when user saves a bag then the user would get a ticket and then the bag would be saved in locker 2
-//    given a robot which manages one empty locker and another full locker(locker 1 capacity: 1, locker 2 capacity: 1) when user saves a bag then the user would get a ticket and then the bag would be saved in locker 1
-//    given a robot which manages two full lockers(capacity: 1) when user saves a bag then the user would get an error message('柜子已满')
-//    given a valid ticket when retrieves bag then user would get the bag.
-//    given an invalid ticket when retrieves bag then user would get error message('票据无效').
-//    given an used ticket when retrieves bag then user would get error message('票据无效').
     @Test
     void should_get_two_different_tickets_when_save_two_bags_given_a_robot_with_two_empty_lockers() {
         SmartLockerRobot smartLockerRobot = (SmartLockerRobot) LockerRobotHelper.createDefaultSmartLockerRobot()
@@ -48,6 +42,28 @@ class SmartLockerRobotTest {
         smartLockerRobot.save(bag);
         Locker locker2 = LockerRobotHelper.getLocker(NUMBER_TWO);
         assertTrue(locker2.hasBag(bag));
+    }
 
+    //    given a robot which manages one empty locker and another full locker(locker 1 capacity: 1, locker 2 capacity: 1) when user saves a bag then the user would get a ticket and then the bag would be saved in locker 1
+//    given a robot which manages two full lockers(capacity: 1) when user saves a bag then the user would get an error message('柜子已满')
+//    given a valid ticket when retrieves bag then user would get the bag.
+//    given an invalid ticket when retrieves bag then user would get error message('票据无效').
+//    given an used ticket when retrieves bag then user would get error message('票据无效').
+
+
+    @Test
+    void should_save_the_bag_in_locker1_when_save_bag_given_a_robot_with_empty_locker_and_full_locker() {
+        SmartLockerRobot smartLockerRobot = (SmartLockerRobot) LockerRobotHelper.createDefaultSmartLockerRobot()
+                .withLocker(1)
+                .withLocker(1)
+                .build();
+
+        Ticket ticket = smartLockerRobot.save(new Bag());
+        smartLockerRobot.save(new Bag());
+        smartLockerRobot.retrieve(ticket);
+        Bag bag = new Bag();
+        smartLockerRobot.save(bag);
+        Locker locker1 = LockerRobotHelper.getLocker(NUMBER_ONE);
+        assertTrue(locker1.hasBag(bag));
     }
 }
