aea964ef4ea7e33016e51f648b7fd303370f6c92
hongyu.li <hongyu.li@thoughtworks.com>
2020-04-15 13:50:04 +0800

    [feat]: given a robot which manages two lockers when user saves a bag then the user would get a ticket and then the bag would be saved in locker 2.

--- src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerRobotTest.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/SuperLockerRobotTest.java
@@ -1,15 +1,13 @@
 package cn.xpbootcamp.gilded_rose;
 
 
-import net.bytebuddy.implementation.bind.annotation.Super;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-//given a robot which manages two empty lockers(capacity: 1) when user saves a bag then the user would get a ticket and then the bag would be saved in locker 1
-//given a robot which manages two lockers(locker 1 capacity: 2 saved 1, locker 2 capacity: 1) when user saves a bag then the user would get a ticket and then the bag would be saved in locker 2
+
 //given a robot which manages two full lockers(capacity: 1) when user saves a bag then the user would get an error message('柜子已满')
 //given a valid ticket when retrieves bag then user would get the bag.
 //given an invalid ticket when retrieves bag then user would get error message('票据无效').
@@ -31,5 +29,19 @@ class SuperLockerRobotTest {
         assertNotNull(ticket);
         Locker locker1 = LockerRobotHelper.getLocker(NUMBER_ONE);
         assertTrue(locker1.hasBag(bag));
+    }
+
+    @Test
+    void should_get_ticket_when_saving_bag_given_a_robot_manages_two_lockers_having_50_and_100_available_ratio() {
+        SuperLockerRobot superLockerRobot = (SuperLockerRobot) LockerRobotHelper.createDefaultSuperLockerRobot()
+                .withLocker(2)
+                .withLocker(1)
+                .build();
+        superLockerRobot.save(new Bag());
+        Bag bag = new Bag();
+        Ticket ticket = superLockerRobot.save(bag);
+        assertNotNull(ticket);
+        Locker locker2 = LockerRobotHelper.getLocker(NUMBER_TWO);
+        assertTrue(locker2.hasBag(bag));
     }
 }
